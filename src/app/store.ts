e96b04cbf0b7214b6030b468c56051e0e42fde96
DatDT19101998 <datdt@reactplus.com>
2022-03-17 09:06:42 +0700

    feat: config redux-saga

---
+++ src/app/store.ts
@@ -0,0 +1,23 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+import createSagaMidleware from 'redux-saga';
+import rootSaga from './rootSaga';
+
+const sagaMidleware = createSagaMidleware();
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMidleware),
+});
+sagaMidleware.run(rootSaga);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
