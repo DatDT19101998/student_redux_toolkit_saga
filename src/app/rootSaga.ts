16ecd96cbb9e35edc3e065cb8b02a1edbae9e99d
DatDT19101998 <datdt@reactplus.com>
2022-03-18 07:56:01 +0700

    feat: config routing app

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,10 +1,6 @@
 import counterSaga from 'features/counter/countSaga';
 import { all } from '@redux-saga/core/effects';
 
-function* helloSaga() {
-  console.log('Hello Saga');
-}
-
 export default function* rootSaga() {
-  yield all([helloSaga(), counterSaga()]);
+  yield all([counterSaga()]);
 }
