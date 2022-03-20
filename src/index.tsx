00c507d82f2ea52d34e16f0f2f4862f18ac697fc
DatDT19101998 <datdt@reactplus.com>
2022-03-20 08:39:16 +0700

    feat: toastify

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 import { history } from 'utils';
 import AppRouter from './App';
 import { store } from './app/store';
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,6 +17,17 @@ ReactDOM.render(
         <CssBaseline />
         <AppRouter />
       </ConnectedRouter>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
